0040df4521ab42b9de702b219edc93ca60401dd8
oceanicoxen <etbender@icloud.com>
2022-11-05 16:06:37 -0400

    changed some variable names so they weren't trash

--- src/main/java/oceanicvoid/thedeeperdark/items/questbook/EntrenchedJournal.java
+++ src/main/java/oceanicvoid/thedeeperdark/items/questbook/EntrenchedJournal.java
@@ -54,7 +54,7 @@ public class EntrenchedJournal extends Item {
 	}
 
 	@Override
-	public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> tooltip, TooltipFlag p_41424_) {
+	public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag tooltipFlag) {
 		LoreItem.addLore(ID, tooltip);
 	}
 
