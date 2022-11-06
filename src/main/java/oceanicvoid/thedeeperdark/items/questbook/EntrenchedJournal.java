12a0ce2c9c20c22dc2f07d41629b8e6ba489a99b
oceanicoxen <etbender@icloud.com>
2022-11-05 20:08:51 -0400

    Added the Entrenched Journal to a creative tab

--- src/main/java/oceanicvoid/thedeeperdark/items/questbook/EntrenchedJournal.java
+++ src/main/java/oceanicvoid/thedeeperdark/items/questbook/EntrenchedJournal.java
@@ -6,10 +6,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.network.NetworkHooks;
 import oceanicvoid.thedeeperdark.TheDeeperDark;
@@ -37,6 +34,7 @@ public class EntrenchedJournal extends Item {
 	private static Properties addProperties(Properties properties) {
 		properties.stacksTo(1);
 		properties.rarity(Rarity.EPIC);
+		properties.tab(CreativeModeTab.TAB_TOOLS);
 		return properties;
 	}
 
